936f8784e7c45f08ca49ab78582687b2abc28308
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-05 18:33:10 +0900

    Add create method into groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,4 +4,12 @@ class GroupsController < ApplicationController
   def new
     @users = User.all
   end
+
+  def create
+    group = Group.new
+    group.name = params[:chat_group][:name]
+    group.save
+
+    redirect_to root_path
+  end
 end
