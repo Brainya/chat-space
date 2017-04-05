ea87d1fb71b34c281c75fbcd6a5176f47bf6e583
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-05 13:16:46 +0900

    Migrate groups table

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 20170405032139) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
