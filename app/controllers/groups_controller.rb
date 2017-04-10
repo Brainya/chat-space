f52301a7d577914e653b9a300ded21a02fd4e9f2
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-11 02:31:38 +0900

    Modify error detections in create and update action at group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,41 +21,43 @@ class GroupsController < ApplicationController
     @group.attributes = {name: create_params[:name]}
     user_ids = create_params[:user_ids].drop(1)
 
-    if !@group.save
+    if user_ids.length < 1
+      render :edit, inline: "メンバーが選択されていません"
+      return
+    end
+
+    if @group.save
+      user_ids.each do |id|
+        user = User.find(id)
+        UserGroup.where(user_id: user.id, group_id: @group.id).first_or_create
+      end
+
+      UserGroup.where(group_id: @group.id).where.not(user_id: user_ids).destroy_all
+      redirect_to action: :show, id: @group.id
+    else
       render :edit, inline: @group.errors.full_messages[0]
-      return
-    elsif user_ids.length < 1
-      render :edit, inline: "ユーザーを選択してください"
-      return
     end
-
-    user_ids.each do |id|
-      user = User.find(id)
-      UserGroup.where(user_id: user.id, group_id: @group.id).first_or_create
-    end
-
-    UserGroup.where(group_id: @group.id).where.not(user_id: user_ids).destroy_all
-    redirect_to action: :show, id: @group.id
   end
 
   def create
     group = Group.new(name: create_params[:name])
     user_ids = create_params[:user_ids].drop(1)
 
-    if !group.save
+    if user_ids.length < 1
+      render :new, inline: "メンバーが選択されていません"
+      return
+    end
+
+    if group.save
+      user_ids.each do |id|
+        user = User.find(id)
+        UserGroup.create(user_id: user.id, group_id: group.id)
+      end
+
+      redirect_to action: :show, id: group.id
+    else
       render :new, inline: group.errors.full_messages[0]
-      return
-    elsif user_ids.length < 1
-      render :new, inline: "ユーザーを選択してください"
-      return
     end
-
-    user_ids.each do |id|
-      user = User.find(id)
-      UserGroup.create(user_id: user.id, group_id: group.id)
-    end
-
-    redirect_to action: :show, id: group.id
   end
 
   private
