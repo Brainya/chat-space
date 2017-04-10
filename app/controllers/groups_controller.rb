0a512226f65ae123cb0924dc6ff47473640118c9
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-10 13:41:08 +0900

    Remove a hash rocket from create params action at groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -65,7 +65,6 @@ class GroupsController < ApplicationController
 
   private
   def create_params
-    params.require(:group).permit(:name, :user_ids => [])
     params.require(:group).permit(:name, user_ids: [])
   end
 
