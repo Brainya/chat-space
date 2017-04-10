8b1088c8e8cb935bef32389e1fe48e3f570a0f6e
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-10 13:40:30 +0900

    Add update action at groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,24 +12,36 @@ class GroupsController < ApplicationController
     @members_string = "Members: #{@users.pluck(:name).join(", ")}"
   end
 
-    if name.empty?
-      redirect_to new_group_path, alert: "グループ名が入力されていません。"
-      return
-    elsif groups.exists?(name: name)
-      redirect_to new_group_path, alert: "既に同じ名前のグループがあります。"
   def edit
     @group = Group.find(params[:id])
     @users = User.all
   end
-      return
-    elsif user_ids.length <= 0
-      redirect_to new_group_path, alert: "チャットメンバーが選択されていません。"
+
+  def update
+    group = Group.find(params[:id])
+    group.name = create_params[:name]
+
+    unless group.save || user_ids_check_boxes_validation
+      redirect_to edit_group_path, id: group.id, alert: group.errors.full_messages[0]
       return
     end
 
-    group = Group.new
-    group.name = name
-    group.save
+    user_ids = create_params[:user_ids].drop(1)
+
+    unless user_ids_check_boxes_validation(group, user_ids)
+      redirect_to edit_group_path, id: group.id, alert: group.errors.full_messages[0]
+      return
+    end
+
+    user_ids.each do |id|
+      user = User.find(id)
+      UserGroup.where(user_id: user.id, group_id: group.id).first_or_create
+    end
+
+    UserGroup.where(group_id: group.id).where.not(user_id: user_ids).destroy_all
+    redirect_to action: :show, id: group.id
+  end
+
   def create
     group = Group.new(name: create_params[:name])
     user_ids = create_params[:user_ids].drop(1)
