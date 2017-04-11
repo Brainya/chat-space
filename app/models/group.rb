248112ab6a757bbf000a5de06c6ae83423a8963b
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-11 20:22:16 +0900

    Add user ids validation into group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,10 @@
 class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validate :user_ids_validation
   has_many :user_groups
   has_many :users, through: :user_groups
-  accepts_nested_attributes_for :users
+
+  def user_ids_validation
+    errors.add(:name, "メンバーが選択されていません") unless user_ids.present?
+  end
 end
