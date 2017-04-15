ece93250d1689f69c09d34817fccecba116292f4
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-15 15:24:01 +0900

    Modify to display groups recent messages in left column

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -6,4 +6,8 @@ module ChatsHelper
   def get_japanese_datetime_by_datetime(datetime)
     datetime.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[datetime.wday]}) %X")
   end
+
+  def get_recent_message_by_group(group)
+    group.messages.order(:created_at).reverse_order.pluck(:message).first
+  end
 end
