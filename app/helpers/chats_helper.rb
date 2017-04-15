7497f65f9af58bcca0c4dc85889dfbd86fe599ea
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-15 15:41:40 +0900

    Shorten functions name

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,13 +1,13 @@
-module ChatsHelper
-  def get_members_string_by_group(group)
+module ChatsHelpers
+  def get_group_members(group)
     "Members: #{group.users.pluck(:name).join(", ")}"
   end
 
-  def get_japanese_datetime_by_datetime(datetime)
+  def get_datetime_jp(datetime)
     datetime.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[datetime.wday]}) %X")
   end
 
-  def get_recent_message_by_group(group)
+  def get_recent_message(group)
     group.messages.order(:created_at).reverse_order.pluck(:message).first
   end
 end
