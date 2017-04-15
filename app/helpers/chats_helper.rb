01a22cd917b1cbe0861df74ca2f213c90bace4a5
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-15 15:22:03 +0900

    Modify function name

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,9 +1,9 @@
 module ChatsHelper
-  def members_string
-    "Members: #{@group.users.pluck(:name).join(", ")}"
+  def get_members_string_by_group(group)
+    "Members: #{group.users.pluck(:name).join(", ")}"
   end
 
-  def japanese_datetime(datetime)
+  def get_japanese_datetime_by_datetime(datetime)
     datetime.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[datetime.wday]}) %X")
   end
 end
