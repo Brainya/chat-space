c221b42762ae0a18d73ef7b443a1a5ac17f2c9b6
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-15 13:19:38 +0900

    Move messages datetime to chats helper

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -1,2 +1,9 @@
 module ChatsHelper
+  def members_string
+    "Members: #{@group.users.pluck(:name).join(", ")}"
+  end
+
+  def japanese_datetime(datetime)
+    datetime.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[datetime.wday]}) %X")
+  end
 end
