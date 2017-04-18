dc2a1298cc26400d145e7aeb416df644eb791d62
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-18 16:14:14 +0900

    Split a model to 3 models

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,15 +1,5 @@
 FactoryGirl.define do
 
-  factory :user do
-    email    { Faker::Internet.email }
-    password { Faker::Internet.password(8) }
-    name     { Faker::Name::name }
-  end
-
-  factory :group do
-    name { Faker::Name::name }
-  end
-
   factory :message do
     message "hello world"
     user
