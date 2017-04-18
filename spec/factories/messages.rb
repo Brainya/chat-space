88973eb2c6af3656e9cee28b169d82786861222b
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-18 15:56:27 +0900

    Add messages.rb

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,9 +1,19 @@
 FactoryGirl.define do
 
+  factory :user do
+    email    { Faker::Internet.email }
+    password { Faker::Internet.password(8) }
+    name     { Faker::Name::name }
+  end
+
+  factory :group do
+    name { Faker::Name::name }
+  end
+
   factory :message do
-    message              "hello world"
-    user_id              "1"
-    group_id             "1"
+    message "hello world"
+    user
+    group
   end
 
 end
