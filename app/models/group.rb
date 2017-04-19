05bf7f978add67ba3ac8a78198393e080b376a2f
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-19 12:30:18 +0900

    Restore user_ids validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  # validate :user_ids_validation
+  validate :user_ids_validation
   has_many :user_groups
   has_many :users, through: :user_groups
   has_many :messages
