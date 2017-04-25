81bbbb08b2bdea56ae4210457988f828795f5399
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-25 15:55:25 +0900

    Add scrollToBottomMessageList method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,17 @@
 $(function() {
-  function buildHTML(message) {
-    var html = $('<li class="message-list__item">').append(message.content);
+  function scrollToBottomMessageList() {
+    $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight }, 'normal');
+  }
+
+  function buildHTML(data) {
+    var username = $(`<span class="username">`).append(data.username);
+    var date = $(`<span class="date">`).append(data.date);
+    var message = $(`<p class="text">`).append(data.message);
+
+    var header = $(`<div class="message-list__item--header">`).append(username, date);
+    var body = $(`<div class="message-list__item--body">`).append(message);
+    var html = $(`<li class="message-list__item">`).append(header, body);
+
     return html;
   }
 
