4ca8bdf68fd8bfb7a72ce6f9ea6a90845c0045f2
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-29 19:13:03 +0900

    Fix indents

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -10,18 +10,18 @@ $(document).on('turbolinks:load', function() {
     );
     
     return $(this).each(function() {
-      if ($(this).parent().find('.flashMessage').get(0))
+      if ($(this).parent().find('.flash_message').get(0))
       return;
-        
-        var message = $(`<span class="flashMessage ${options.class_name}">`).append(options.text).hide().fadeIn('fast');
-        
-        $(this)[options.how](message);
-        
-        message.delay(options.time).fadeOut('normal', function() {
-          $(this).remove();
-        });
+      
+      var message = $(`<span class="flash_message ${options.class_name}">`).append(options.text).hide().fadeIn('fast');
+      
+      $(this)[options.how](message);
+      
+      message.delay(options.time).fadeOut('normal', function() {
+        $(this).remove();
       });
-    };
+    });
+  };
     
   function scrollToBottomMessageList() {
     $('.message-list').animate({
