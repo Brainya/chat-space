f89f43b4a4378a4a5a4f44e05417502a93eb7180
Brainya <user@Hirokis-MacBook-Pro.local>
2017-04-29 14:33:18 +0900

    Change all group.js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,10 +1,60 @@
-$(document).on('ready turbolinks:load', function() {
-  $('.user-list-input').select2({
-    placeholder: '追加するメンバーを選んでください',
-    allowClear: true,
-    minimumInputLength: 1,
-    language: {
-      inputTooShort: null
+$(document).on('turbolinks:load', function() {
+  var user_list_selected = 'user-list-selected';
+  var user_list_result = 'user-list-result';
+
+  $('form').attr('autocomplete', 'off');
+
+  function getUsernamesByButtons(buttons) {
+    var usernames = [];
+
+    $.each(buttons, function() {
+      usernames.push($(this).attr('id').replace('button-', '').replace('-selected', ''));
+    });
+
+    return usernames
+  };
+
+  $(`#${user_list_selected}`).on('click', 'button', function() {
+    $(this).remove();
+  });
+
+  $(`#${user_list_result}`).on('click', 'button', function() {
+    var id = $(this).attr('id');
+
+    $(this).attr('id', `${id}-selected`).appendTo(`#${user_list_selected}`);
+  });
+
+  $('form').on('submit', function() {
+    var usernames = getUsernamesByButtons($(`#${user_list_selected} button`));
+    $('<input>').attr('type', 'hidden').attr('name', 'usernames').attr('value', usernames).appendTo(this);
+  });
+
+  $('#user-list-input').on('keyup', function() {
+    var preFunc = null;
+    var preInput = '';
+    var input = '';
+    input = $.trim($(this).val());
+    
+    if (preInput !== input) {
+      clearTimeout(preFunc);
+      preFunc = setTimeout($.ajax({
+        url: 'ajax_user_list',
+        type: 'GET',
+        data: (`q=${input}`)
+      }), 500);
     }
+    
+    preInput = input;
+  });
+
+  $(`#${user_list_result}`).on('DOMSubtreeModified propertychange', function() {
+    var resultUsernames = getUsernamesByButtons($(`#${user_list_result} button`));
+    var selectedUsernames = getUsernamesByButtons($(`#${user_list_selected} button`));
+    
+    $.each(resultUsernames, function(index, value) {
+      if ($.inArray(value, selectedUsernames) != -1) {
+        $(`#button-${value}`).remove();
+      }
+    });
   });
 });
